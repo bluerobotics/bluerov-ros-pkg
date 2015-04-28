881c7b4ff14d21e0a2d0c57076f5f57db72e953b
Josh Villbrandt <josh@javconcepts.com>
2015-04-27 19:06:18 -0700

    Add pilot program that publishes 0% thrust to six thrusters

--- docs/Usage.md
+++ docs/Usage.md
@@ -7,7 +7,7 @@ Once you have completed [Setup](Setup.md), this guide will show you have to laun
 Launching the BlueROV core services can be as simple as:
 
 ```bash
-ssh viki@bluerov -p wiki -c 'roslaunch core'
+ssh viki@bluerov -p viki -c 'roslaunch bluerov core.launch'
 ```
 
 If you're in the middle of development, however, you'll likely want to turn on services with a bit more granularity. For starters, run the follow three commands in three separate terminal windows:
@@ -17,22 +17,44 @@ If you're in the middle of development, however, you'll likely want to turn on s
 roscore
 
 # window 2
-roslaunch serial
+roslaunch bluerov serial.launch
 
 # window 3
-roslaunch pilot
+roslaunch bluerov pilot.launch
 ```
-
-TODO
 
 ## Ground Station Tele-Operation
 
 TODO: set remote IP, launch xbox_teleop
 
-## Interrogating Active Nodes
+## Interrogating ROS
 
-TODO: rosgraph, etc
+ROS includes a variety of tools to interrogate a running system. This is a brief overview on a few of them.
 
-If everything is working correctly, you should see a graph structure similar to this:
+## rostopic
+
+The [rostopic](http://wiki.ros.org/rostopic?distro=indigo) command will list and display ROS network messages from the command line. With the BlueROV core services running, try the follow commands:
+
+```bash
+rostopic list
+rostopic hz thruster
+rostopic echo thruster
+```
+
+## rqt_graph
+
+The [rqt_graph](http://wiki.ros.org/rqt_graph?distro=indigo) command displays all active nodes and shows the message subscribe/publish relationships between them. With all of the BlueROV nodes turned on, you should see a graph structure similar to this:
+
+TODO: graph image
+
+## rqt_plot
+
+The [rqt_plot](http://wiki.ros.org/rqt_plot) command is a quick and dirty way to plot ROS message data over time. Try the follow command with the `pilot` node running:
+
+```bash
+rqt_plot /thruster/commands/data[0] /thruster/commands/data[1] /thruster/commands/data[2] /thruster/commands/data[3] /thruster/commands/data[4] /thruster/commands/data[5]
+```
+
+## rviz
 
 TODO
--- src/pilot.cpp
+++ src/pilot.cpp
@@ -1,85 +1,87 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-
-#include <sstream>
-
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
+/*
+ * File: bluerov/src/pilot.cpp
+ * Author: Josh Villbrandt <josh@javconcepts.com>
+ * Date: April 2015
+ * Description: Sends thruster commands in microseconds to the BlueROV.
  */
-int main(int argc, char **argv)
+
+#include <ros/ros.h>
+#include <bluerov/Thruster.h>
+
+class Pilot
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line. For programmatic
-   * remappings you can use a different version of init() which takes remappings
-   * directly, but for most command-line programs, passing argc and argv is the easiest
-   * way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  ros::init(argc, argv, "pilot");
+  public:
+    Pilot();
+    void spin();
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  ros::NodeHandle n;
+  private:
+    ros::NodeHandle nh;
+    ros::Publisher thruster_pub;
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+    int n_thrusters;
+    double pub_rate;
+    ros::Time last_pub_time;
+    ros::Duration pub_period;
 
-  ros::Rate loop_rate(10);
+    void sendThrusterMessage();
+};
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
-  while (ros::ok())
+Pilot::Pilot()
+{
+  // load parameters
+  ros::NodeHandle nh_priv("~");
+  nh_priv.param("n_thrusters", n_thrusters, 1); // amount
+  nh_priv.param("pub_rate", pub_rate, 10.0); // Hz
+
+  // lets show em what we got
+  ROS_INFO_STREAM("param n_thrusters: " << n_thrusters);
+  ROS_INFO_STREAM("param pub_rate: " << pub_rate);
+
+  // connects subs and pubs
+  thruster_pub = nh.advertise<bluerov::Thruster>("thruster", 1);
+
+  // setup
+  last_pub_time = ros::Time::now();
+  pub_period = ros::Duration(1.0 / pub_rate);
+}
+
+void Pilot::spin()
+{
+  while(ros::ok())
   {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-    std_msgs::String msg;
+    // time to publish?
+    if(ros::Time::now() > (last_pub_time + pub_period)) {
+      sendThrusterMessage();
+      last_pub_time = ros::Time::now();
+    }
 
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-
-    ROS_INFO("%s", msg.data.c_str());
-
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
-    chatter_pub.publish(msg);
-
+    // call all waiting callbacks
     ros::spinOnce();
+  }
+}
 
-    loop_rate.sleep();
-    ++count;
+void Pilot::sendThrusterMessage()
+{
+  // create message
+  bluerov::Thruster thruster_msg;
+  thruster_msg.header.stamp = ros::Time::now();
+  thruster_msg.header.frame_id = "base_link";
+
+  // set motor commands
+  for (int i = 0; i < n_thrusters; i++)
+  {
+    // thruster values should be between 1100 and 1900 microseconds (us)
+    // values less than 1500 us are backwards; values more than are forwards
+    thruster_msg.commands.data.push_back(1500);
   }
 
+  // publish message
+  thruster_pub.publish(thruster_msg);
+}
 
-  return 0;
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "pilot");
+  Pilot pilot;
+  pilot.spin();
 }
